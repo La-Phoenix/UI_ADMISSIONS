9376c3620ef0a502ef7a247d2f3d719acb16c7d2
La-Phoenix <samuelokundalaiye@gmail.com>
2024-03-29 00:25:50 +0100

    Modified Olevel page

--- src/Components/FileUploadInput/FileUploadInput.jsx
+++ src/Components/FileUploadInput/FileUploadInput.jsx
@@ -1,37 +1,35 @@
-import { Button, FormControl, FormHelperText, Input, InputLabel } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import React, { useEffect, useRef } from "react";
 import "./FileUploadInput.css"
 
-const FileUploadInput = ({label, addFile, resetFlag, ml }) => {
-    // const [fileName, setFileName] = useState("");
-    // const fileInputRef = useRef(null);
-    
-    // const handleFileChange = (event) => {
-    //     const file = event.target.files[0];
-    //     if (file) {
-    //         setFileName(file.name);
-    //         addFile(fieldName,file)
-    //     }
-    // };
+const FileUploadInput = ({label, addFile, resetFlag, ml, fieldName}) => {
+    const fileInputRef = useRef(null);
 
-    // useEffect(() => {
-    //     if (fileInputRef.current){
-    //         fileInputRef.current.value = ""
-    //         setFileName("")
-    //     }
-    // }, [resetFlag])
     
-    // const handleClick = () => {
-    //     fileInputRef.current.click();
-    // };
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            addFile(fieldName, file)
+        }
+    };
+
+    useEffect(() => {
+        if (resetFlag){
+            fileInputRef.current.value = '';
+        }
+    }, [resetFlag])
+
     
     return (
         <div style={{marginBottom: "1.5rem"}}>
             <label style={{marginRight: "4rem", fontSize: "24px", fontWeight: "500", color: "rgb(83, 81, 81)"}}>
                 {label}
             </label>
-            <input style={{marginLeft: ml}} type="file" accept="image/*"></input>
+            <input
+            style={{marginLeft: ml}} 
+            id='fileInput'
+            ref={fileInputRef}
+            type="file" accept="image/*, .pdf" 
+            onChange={handleFileChange}></input>
         </div>
     );
   }
--- src/Pages/OlevelVerify/OlevelVerify.jsx
+++ src/Pages/OlevelVerify/OlevelVerify.jsx
@@ -1,43 +1,135 @@
-import { Breadcrumbs, Button, Container, Typography } from "@mui/material";
-import React from "react";
+import { Box, Breadcrumbs, Button, Container, Modal, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./OlevelVerify.css"
 import FileUploadInput from "../../Components/FileUploadInput/FileUploadInput";
+import axios from "axios";
+import { APIBASEURL } from "../../App";
+import SnackbarComp from "../../Components/SnackBar/SnackBar";
+
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    height: "20%",
+    backgroundColor: 'white',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    boxShadow: 24,
+    p: 4,
+  }
 
 const OlevelVerify = () => {
+    const user = JSON.parse(localStorage.getItem("Profile"));
+    const [resetFlag, setResetFlag ] = useState(false)
+    const [open, setOpen ] = useState(false)
+    const [openSnack, setOpenSnack ] = useState(false)
+    const [snackBarMsg, setSnackBarMsg] = useState(false);
+    const [snackBarColor, setSnackBarColor] = useState('');
+
+    const [isDocSubmitted, setIsDocSubmitted ] = useState(false)
+    let formData = new FormData()
+
+    const submit = async () => {
+        try {
+            const resp = await axios.post(`${APIBASEURL}/user/olevelverify`, formData, {
+                headers: {
+                    Authorization: `Bearer ${user.token}`
+                }
+            })
+            setSnackBarMsg(resp.data.message)
+            setSnackBarColor('')
+            setOpenSnack(true);
+            setIsDocSubmitted(true)
+        } catch (error) {
+            if (error.response && error.response.data){
+                setSnackBarMsg(error.response.data.message)
+                setOpenSnack(true);
+            } else {
+                setSnackBarMsg("Something Went Wrong. Please try again later.")
+                setOpenSnack(true);
+            }
+            setSnackBarColor('red')
+        }
+        setResetFlag(prev => !prev)
+    }
+
+    const addFile = (fieldName, file) => {
+        formData.set(fieldName, file)
+    }
+
     return (
-        <Container maxWidth={false}  sx={{ height: "100%"}}>
-            <Breadcrumbs separator={<span style={{fontSize: "50px"}}>/</span>} aria-label="breadcrumb">
-                <Link style={{textDecoration: "none"}} href="/Home">
-                    <Typography variant="h3" color="blue">Home</Typography>
-                </Link>
-                <Typography variant="h3" color="blue">Verify O/levels</Typography>
-            </Breadcrumbs>
-            <div style={{ height:"50%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <form>
-                    {[{
-                        name: "WAEC/NECO",
-                        ml: "1.8rem"
-                    }, {
-                        name: "SCRATCH CARD",
-                        ml: "0"
-                    }].map((item, index) => (
-                        <FileUploadInput key={index} label={item.name} ml={item.ml} />
-                    ))}
-                    <div style={{width: "100%", display: "flex", justifyContent: "center", marginTop: "3rem"}}>
+        <>
+            <SnackbarComp open={openSnack} setOpen={setOpenSnack} snackBarMsg= {snackBarMsg} snackBarColor={snackBarColor} />
+            {
+                isDocSubmitted ? (
+                    <div style={{height: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
                         <Button 
                             variant="contained"
-                            sx={{background: "rgb(105, 101, 101)", width: "30%", ":hover": {
-                                background: "rgb(83, 81, 81)"
-                            }, borderRadius: "10px"}}
+                            sx={{background: "rgb(105, 101, 101)", width: "25%", height: "15%", fontSize: "34px", fontWeight: "600", ":hover": {
+                                    background: "rgb(83, 81, 81)"
+                                }, borderRadius: "10px"}}
                             size="large"
+                            onClick={() => setOpen(true)}
                             >
-                            Verify
+                            CHECK STATUS
                         </Button>
+                        <Modal
+                            open={open}
+                            onClose={() => setOpen(false)}
+                        >
+                            <Box style={modalStyle}>
+                                <Typography id="modal-modal-title" variant="h6" component="h2">
+                                    O/LEVEL VERIFICATION STATUS
+                                </Typography>
+                                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                                    IN PROGRESS.
+                                </Typography>
+                            </Box>
+                        </Modal>
                     </div>
-                </form>
-            </div>
-        </Container>
+                ) : (
+                    <Container maxWidth={false}  sx={{ height: "100%"}}>
+                        <Breadcrumbs separator={<span style={{fontSize: "50px"}}>/</span>} aria-label="breadcrumb">
+                            <Link style={{textDecoration: "none"}} href="/Home">
+                                <Typography variant="h3" color="blue">Home</Typography>
+                            </Link>
+                            <Typography variant="h3" color="blue">Verify O/levels</Typography>
+                        </Breadcrumbs>
+                        <div style={{ height:"50%", width: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
+                            <form>
+                                {[{
+                                    name: "WAEC/NECO",
+                                    fieldName: "WAEC_NECO",
+                                    ml: "1.8rem"
+                                }, {
+                                    name: "SCRATCH CARD",
+                                    fieldName: "SCRATCH_CARD",
+                                    ml: "0"
+                                }].map((item, index) => (
+                                    <FileUploadInput key={index} label={item.name} fieldName={item.fieldName} ml={item.ml} addFile={addFile} resetFlag={resetFlag} />
+                                ))}
+                                <div style={{width: "100%", display: "flex", justifyContent: "center", marginTop: "3rem"}}>
+                                    <Button 
+                                        variant="contained"
+                                        sx={{background: "rgb(105, 101, 101)", width: "30%", ":hover": {
+                                            background: "rgb(83, 81, 81)"
+                                        }, borderRadius: "10px"}}
+                                        size="large"
+                                        onClick={submit}
+                                        >
+                                        Verify
+                                    </Button>
+                                </div>
+                            </form>
+                        </div>
+                    </Container>
+                )
+            }
+        </>
     )
 }
 
